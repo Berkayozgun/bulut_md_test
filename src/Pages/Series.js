540b32192a1c2982df8530d304ca8a36d44bef2c
Berkay Özgün <berkayozgun001@gmail.com>
2022-08-02 14:39:01 +0300

    Series sayfasına da sıralama fonksiyonu eklendi

--- src/Pages/Series.js
+++ src/Pages/Series.js
@@ -1,15 +1,40 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import database from "../Dataset/database.json";
 import { TextField } from "@mui/material";
 import SeriesList from "../Components/SeriesList";
 import Box from "@mui/material/Box";
+import InputLabel from "@mui/material/InputLabel";
+import MenuItem from "@mui/material/MenuItem";
+import FormControl from "@mui/material/FormControl";
+import Select from "@mui/material/Select";
 
 function Series(props) {
   const [inputText, setInputText] = useState("");
+
   let inputHandler = (e) => {
     //convert input text to lower case
     var lowerCase = e.target.value.toLowerCase();
     setInputText(lowerCase);
+  };
+
+  const [data, setData] = useState([]);
+  const [sortType, setSortType] = useState([]);
+  useEffect(() => {
+    if (sortType === null) {
+      setData(database.entries);
+    } else if (sortType === "asc") {
+      database.entries.sort((a, b) => {
+        return b.releaseYear - a.releaseYear;
+      });
+    } else if (sortType === "desc") {
+      database.entries.sort((a, b) => {
+        return a.releaseYear - b.releaseYear;
+      });
+    } else setData(database.entries);
+  }, [sortType]);
+
+  const handleChange = (e) => {
+    setSortType(e.target.value);
   };
 
   return (
@@ -29,6 +54,27 @@ function Series(props) {
           fullWidth
           label="Film / Dizi / Oyuncu ara"
         />
+        <FormControl
+        sx={{
+          width: "15rem",
+          height: "3.5rem",
+          justifyContent: "flex-start",
+          marginTop: "2.5rem",
+          marginLeft: "29.5rem",
+        }}
+      >
+        <InputLabel id="demo-simple-select-label">Sırala</InputLabel>
+        <Select
+          labelId="demo-simple-select-label"
+          id="demo-simple-select"
+          value={sortType}
+          label="age"
+          onChange={handleChange}
+        >
+          <MenuItem value="asc">Eskiye Göre Sırala</MenuItem>
+          <MenuItem value="desc">Yeniye Göre Sırala</MenuItem>
+        </Select>
+      </FormControl>
       
       <SeriesList input={inputText} />
     </Box>
